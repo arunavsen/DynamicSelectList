5a34517a3832bdef18ff53772f3b9cc5d8913be0
Arunav <arunav@vonome.com>
2021-04-03 14:59:03 +0600

    Add ApplicationDbContext class

---
+++ DynamicSelectList/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DynamicSelectList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DynamicSelectList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
