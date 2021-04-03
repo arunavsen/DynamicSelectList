ca0ded2c779814dc7ac42dd38142927b345567ad
Arunav <arunav@vonome.com>
2021-04-03 16:33:34 +0600

    Add List action with view in Home Controller

--- DynamicSelectList/Controllers/HomeController.cs
+++ DynamicSelectList/Controllers/HomeController.cs
@@ -3,24 +3,37 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using DynamicSelectList.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DynamicSelectList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DynamicSelectList.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            var customerList = _db.Customers
+                .Include(x=>x.Country)
+                .Include(x=>x.Country)
+                .ToList();
+            return View(customerList);
         }
 
         public IActionResult Privacy()
