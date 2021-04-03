c692f02cf267542c63ae111802cfb8a065451683
Arunav <arunav@vonome.com>
2021-04-03 14:40:05 +0600

    Add Customer class

---
+++ DynamicSelectList/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicSelectList.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [DataType(DataType.EmailAddress,ErrorMessage = "Email is not valid")]
+        public string EmailId { get; set; }
+
+        [Required]
+        [ForeignKey("Country")]
+        [MaxLength(3)]
+        [DisplayName("Country")]
+        public string CountryCode { get; set; }
+        public virtual Country Country { get; set; }
+
+        [Required]
+        [ForeignKey("City")]
+        [MaxLength(3)]
+        [DisplayName("City")]
+        public string CityCode { get; set; }
+        public virtual City City { get; set; }
+    }
+}
