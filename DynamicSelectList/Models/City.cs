f34c5266b8a0869579d5af341c1cbd43ef41e63b
Arunav <arunav@vonome.com>
2021-04-03 14:19:51 +0600

    Add City class

---
+++ DynamicSelectList/Models/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicSelectList.Models
+{
+    public class City
+    {
+        [Key]
+        [MaxLength(3)]
+        public string Code { get; set; }
+        [Required]
+        [MaxLength(75)]
+        public string Name { get; set; }
+        [ForeignKey("Cointry")]
+        public string CountryCode { get; set; }
+        public virtual Country Country { get; set; }
+    }
+}
